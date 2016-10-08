48541a94c764a1b87f891c70eaacf3c3005743ea
Cedric van Putten <me@bycedric.com>
2016-10-08 23:00:59 +0200

    Used `self` instead of `this`

--- app/others/sw.js
+++ app/others/sw.js
@@ -23,7 +23,7 @@ const CACHE_URLS = [
 	'/images/apple/icon-192.png',
 ];
 
-this.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open(CACHE_NAME).then(function(cache) {
 			return cache.addAll(CACHE_URLS);
@@ -31,7 +31,7 @@ this.addEventListener('install', function(event) {
 	);
 });
 
-this.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
 	event.respondWith(
 		caches.match(event.request).then(function(response) {
 			return response || fetch(event.request);
