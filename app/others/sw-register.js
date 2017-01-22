d387c9f6673b9ba1edf6cff9a0c8ae884a7ea5d5
Cedric van Putten <me@bycedric.com>
2017-01-22 16:14:24 +0100

    Replaced the old react code with the new polymer code

--- app/others/sw-register.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js')
-		.then(function() { console.log('Service worker up and running!'); })
-		.catch(function(error) { console.log('Failed to register service worker', error); });
-}
--- bootstrap/browser.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-
-import AppLayout from 'app/layouts/app';
-
-/**
- * Render the application into the `body > #app` element.
- */
-render(<AppLayout />, document.getElementById('app'));
