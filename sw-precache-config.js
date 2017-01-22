f63dd44080ebc58fd46004befef6284c29104a2c
Cedric van Putten <me@bycedric.com>
2017-01-22 17:03:28 +0100

    Fixed issue with the bower component and added images

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -3,6 +3,7 @@ module.exports = {
 		'/index.html',
 		'/manifest.json',
 		'/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+		'/images/*'
 	],
 	navigateFallback: '/index.html',
 	navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/]
